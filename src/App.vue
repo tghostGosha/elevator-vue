74ed6da53408ba42b032f4ca8ed4748dd3ebc77a
thostGosha <tghost@mail.ru>
2024-04-07 22:11:07 +0300

    .

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <h1>ffdff</h1>
+
+    </div>
+  </header>
+<ul>
+  <li>
+    floor 1
+    <button>вызов</button>
+    <div class="elevator">
+
+    </div>
+  </li>
+  <li>
+    floor 2
+    <button>вызов</button>
+  </li>
+  <li>
+    floor 3
+    <button>вызов</button>
+  </li>
+  <li>
+    floor 4
+    <button>вызов</button>
+  </li>
+  <li>
+    floor 5
+    <button>вызов</button>
+  </li>
+</ul>
+
+<!--  <RouterView />-->
+</template>
