78229d0af39e0e1b00eef8ce41a2b4cd6163365b
tghostGosha <tghost@mail.ru>
2024-04-09 12:36:26 +0300

    working

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,68 @@
 <script setup lang="ts">
 import {useCounterStore} from "@/stores/counter";
-import type {Store} from "pinia";
 let floor = useCounterStore()
 const floors = [1, 2, 3, 4, 5, 6]
 
-// const movingTime = (floorCall:number, currentFloor:number) =>{
-//   if(floorCall > currentFloor) {
-//     return (floorCall - currentFloor )*1000
-//   }
-//   if(floorCall < currentFloor) {
-//     return (currentFloor - floorCall )*1000
-//   }
-// }
-let count = 1
+
+
+// console.log(floor.floorNum, 'unique')
+// let count = 1
 const moveElevator = (floorCall:number) => {
-  if (count < floorCall) {
-    count++
+  console.log(floorCall, floor.currentLevel, 'floorCall, floor.currentLevel')
+  if (floor.currentLevel < floorCall) {
+    floor.currentLevel++
     setTimeout(()=>{
-      floor.increment()
-      floor.busy = true
-      console.log(floor.busy)
-      moveElevator(floorCall)
-    }, 1000)
-  }
-  if(count > floorCall) {
-    count--
-    setTimeout(()=>{
-      floor.decrease()
       floor.busy = true
       moveElevator(floorCall)
     }, 1000)
   }
-  if(floorCall === floor.floorNum) {
+  if(floor.currentLevel  > floorCall) {
+    floor.currentLevel--
+    setTimeout(()=>{
+      moveElevator(floorCall)
+    }, 1000)
+  }
+  if(floorCall === floor.currentLevel ) {
     setTimeout(()=>{
       floor.busy = false
-      console.log(floor.busy)
+      console.log(floor.floorNum, 'конец')
     }, 3000)
   }
-  return count
+  return floor.currentLevel
 }
+let timer:any = null
+
+const addTurn = (floorCall:number) => {
+  // clearTimeout(timer)
+  let start =  floor.floorNum.length === 0
+  floor.floorNum.push(floorCall)
+  console.log(floor.floorNum, 'addTurn')
+  if(start) {
+    goToLevel()
+  }
+  // timer = setTimeout(()=>{
+  //   goToLevel()
+  // }, 2000)
+
+}
+const goToLevel = () => {
+  if(floor.floorNum.length > 0) {
+    let level:number|undefined = floor.floorNum.shift()
+
+    if(level!=undefined) {
+      let stepsTimer:number=Math.abs((floor.currentLevel - level)*1000)
+      console.log(level, stepsTimer, 'level')
+      moveElevator(level)
+      setTimeout(()=>{
+        goToLevel()
+      },stepsTimer+ 3000)
+
+    }
+  }
+}
+
+
+
 </script>
 
 <template>
@@ -46,9 +70,9 @@ const moveElevator = (floorCall:number) => {
 <ul>
   <li v-for="item in floors" :key="item" >
     floor {{ item }}
-    <button @click="moveElevator(item)" ></button>
+    <button @click="addTurn(item)" ></button>
     <transition name="slide-fade">
-      <div  :class=" (!floor.busy) ? 'elevator' : 'elevator-move' "  v-if="item === floor.floorNum ">
+      <div  :class=" (!floor.busy) ? 'elevator' : 'elevator-move' "  v-if="item === floor.currentLevel ">
 
       </div>
     </transition>
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,16 +1,31 @@
-import { defineStore } from 'pinia'
-
+import {defineStore} from 'pinia'
+interface State {
+  floorNum: number[]
+  currentLevel: number
+  busy: boolean
+}
 export const useCounterStore = defineStore('floor', {
-  state: () => {
-    return { floorNum: 1, busy: false }
+  state: ():State => {
+    return { floorNum: [], currentLevel: 1, busy: false }
   },
-  actions: {
-    increment() {
-      this.floorNum++
-    },
-    decrease () {
-      this.floorNum--
-    }
+  // getters: {
+  //   sortFloors: (state) => {
+  //     state.floorNum = state.floorNum.reduce((acc: any, item) => {
+  //       if (acc.includes(item)) {
+  //         return acc;
+  //       }
+  //       return [...acc, item];
+  //     }, [])
+  //   }
+  // },
 
-  },
+  // actions: {
+  //   increment() {
+  //     this.floorNum[0]+=1
+  //   },
+  //   decrease () {
+  //     this.floorNum[0]-=1
+  //   }
+  //
+  // },
 })
